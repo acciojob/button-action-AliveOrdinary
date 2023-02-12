fc1aa3bdb3f635a2042ae27cbe03b41b5db0728c
AliveOrdinary <19sama18@uohyd.ac.in>
2023-02-12 17:26:32 +0530

    trial3

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,19 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
-
-
-  const handleClick= ()=>{
-    const [isClicked, setIsClicked] = useState(false);
-  }
+  const [showPara, setShowPara] = useState(false);
+  
   return (
     <div className="App" id="main">
       // Do not alter the main div
-      {isClicked && (<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)}
-      
-      <button id="click" onClick={handleClick}>Click me</button>
+      <button id="click" onClick={() => setShowPara(!showPara)}>
+        Show Paragraph
+      </button>
+      {showPara && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+        </p>
+      )}
     </div>
   );
 }
