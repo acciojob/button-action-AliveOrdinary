3e08d43312eddeb13bbadb8fd73cb097e2331610
AliveOrdinary <19sama18@uohyd.ac.in>
2023-02-12 16:38:13 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,17 @@ import './../styles/App.css';
 
 const App = (props) => {
 
+
+  const renderPara= ()=>{
+    document.getElementById("para").style.display = "block";
+  }
   return (
     <div className="App" id="main">
       // Do not alter the main div
+      <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      <button id="click" onClick={renderPara}>Click me</button>
     </div>
   );
 }
 
-export default App
+export default App;
