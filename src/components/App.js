9477b1c9448bd5f5f87e6e0038f0ddb5a4b3e445
AliveOrdinary <19sama18@uohyd.ac.in>
2023-02-22 16:44:54 +0530

    trail4

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 
 const App = (props) => {
   const [showPara, setShowPara] = useState(false);
-  
+
   return (
     <div className="App" id="main">
-      // Do not alter the main div
       <button id="click" onClick={() => setShowPara(!showPara)}>
         Show Paragraph
       </button>
       {showPara && (
         <p id="para">
-          Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
         </p>
       )}
     </div>
   );
-}
+};
 
 export default App;
